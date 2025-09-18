63d747d5d206d38ef68816d1827883f4f7083c99
appchang <info@appchang.com>
2025-09-18 17:18:15 +0700

    add log check env

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI || "";
+console.log("Mongo URI:", MONGODB_URI);
 
 if (!MONGODB_URI) {
   throw new Error("⚠️ Please define MONGODB_URI in .env.local");
@@ -11,7 +12,8 @@ interface MongooseCache {
   promise: Promise<typeof mongoose> | null;
 }
 
-const cached: MongooseCache = (global as { mongoose?: MongooseCache }).mongoose || {
+const cached: MongooseCache = (global as { mongoose?: MongooseCache })
+  .mongoose || {
   conn: null,
   promise: null,
 };
@@ -20,9 +22,11 @@ export async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: "appchang", // ✅ บังคับใช้ DB ที่คุณสร้าง
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        dbName: "appchang", // ✅ บังคับใช้ DB ที่คุณสร้าง
+      })
+      .then((mongoose) => mongoose);
   }
   cached.conn = await cached.promise;
   return cached.conn;
